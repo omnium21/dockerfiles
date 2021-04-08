51e9506a835265d99c70c98119423121db06590c
Ryan Harkin <ryan.harkin@arm.com>
2021-04-08 12:45:12 +0100

    rundocker.sh: set hostname
    This could be a problem if we try to use networking with multiple containers.
    
    Signed-off-by: Ryan Harkin <ryan.harkin@arm.com>

--- rundocker.sh
+++ rundocker.sh
@@ -2,4 +2,4 @@ IMAGE=${1:-"${USER}-ubuntu"}
 PARAMS="$PARAMS -v /arm:/arm"
 PARAMS="$PARAMS -v /data:/data"
 
-docker run ${PARAMS} --user ${USER}:${USER} -t -i ${IMAGE} 
+docker run ${PARAMS} --user ${USER}:${USER} --hostname docker-ubuntu -t -i ${IMAGE}
