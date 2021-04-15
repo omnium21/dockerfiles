e06edb2b0bc8c72d6069ff8b339517f0db26d3e4
Ryan Harkin <ryan.harkin@arm.com>
2021-04-15 13:14:46 +0100

    Add build.sh -o option
    Add an option to remove the existing image with the name we're about to
    build, in effect, overwriting the existing image.
    
    Signed-off-by: Ryan Harkin <ryan.harkin@arm.com>

--- build.sh
+++ build.sh
@@ -11,6 +11,26 @@ cleanup_exit()
 
 export LANG=C
 
+usage()
+{
+	echo "usage: $0 [-o]"
+	echo "       -o    overwrite existing image"
+	exit 1
+}
+
+while getopts ":o" o; do
+  case "$o" in
+    o) OVERWRITE=true ;;
+    h|*) usage ;;
+  esac
+done
+
 image=${USER}-ubuntu
+
+if [ "${OVERWRITE}" = "true" ]; then
+	echo "Removing existing docker image ${image}"
+	docker rmi -f ${image} || true
+fi
+
 docker build --rm --pull --tag=$image --build-arg USER=${USER} .
 echo $image > .docker-tag
