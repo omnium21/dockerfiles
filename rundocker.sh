fb682bc5b9efa6150388e6c8e184a4cf7c0c5f56
Ryan Harkin <ryan.harkin@arm.com>
2021-04-15 15:08:21 +0100

    Add /dev/fuse into the container
    Signed-off-by: Ryan Harkin <ryan.harkin@arm.com>

--- rundocker.sh
+++ rundocker.sh
@@ -6,4 +6,7 @@ IMAGE=${1:-"${USER}-ubuntu"}
 [ -d "/arm"  ] && PARAMS="${PARAMS} -v /arm:/arm"
 [ -d "/data" ] && PARAMS="${PARAMS} -v /data:/data"
 
+# Devices to be added to the container
+[ -c "/dev/fuse" ] && PARAMS="${PARAMS} --cap-add SYS_ADMIN --device /dev/fuse"
+
 docker run ${PARAMS} --user ${USER}:${USER} --hostname docker-ubuntu -t -i ${IMAGE}
